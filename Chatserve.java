d7b0019c582f4aa87bf70a00439b21adc5aa2e97
danielsbonnin <bonnind@oregonstate.edu>
2016-01-31 08:40:03 -0600

    chatserve handles port input

--- Chatserve.java
+++ Chatserve.java
@@ -8,12 +8,40 @@ import java.net.*;
 import java.io.*;
 
 public class Chatserve {
+	public static String Usage = "Usage: $java Chatserve <Port Number>\n";
+	public static int portno;
 
 	public static void main(String[] args) {
+		
 		// TODO Auto-generated method stub
 		System.out.println("chatserver has started.");
+		if (args.length > 1) {
+			System.err.println("Only 1 argument permitted\n" + Usage);
+			System.exit(1);
+		}
+		
+		else if(args.length == 0) {
+			System.err.println("Port argument required\n" + Usage);
+			System.exit(1);
+		}	
+		
+		/* CLI parsing help obtained from 
+		 * docs.oracle.com/javase/tutorial/essential/environment/cmdLineArgs.html*/
+		else {
+			try {
+				portno = Integer.parseInt(args[0]);
+			} catch (NumberFormatException e) {
+				System.err.println("Port argument must be an integer.\n" + Usage);
+				System.exit(1);
+			}
+		}
+		
+		System.out.println("The port number you entered was " + Integer.toString(portno));
+				
+		
+		
 		/* Code help obtained from docs.oracle.com/javase/tutorial/networking/sockets/clientServer.html */
-		try ( 
+		try (
 			    ServerSocket serverSocket = new ServerSocket(4444);
 			    Socket clientSocket = serverSocket.accept();
 			    PrintWriter out =
