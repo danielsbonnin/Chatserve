848199c4d503b9965d3447148b40aebebb720cc3
danielsbonnin <bonnind@oregonstate.edu>
2016-02-07 22:39:36 -0600

    Added comments

--- chatclient.py
+++ chatclient.py
@@ -1,5 +1,4 @@
-#!/usr/bin/env python
-#^
+#!/usr/bin/env python .
 
 """chatclient.py: A simple chat client for class
 
@@ -22,8 +21,8 @@ import errno
 import signal
 
 MSG_END = '<END>'
-"""String: The delimiter to mark end of messages
-"""
+#String: The delimiter to mark end of messages
+
 
 def main():
     signal.signal(signal.SIGTERM, exitCleanly)
@@ -33,31 +32,29 @@ def main():
     print("You are requesting access to the chat server at : " + args.hostname + ':' +
           str(args.port))
 
-    
     handle = getHandle()
 
-    """Create a tcp socket. """
+    #Create a tcp socket. 
     s = socket.socket(
         socket.AF_INET, socket.SOCK_STREAM)
 
-    """Verify Server address and contact server"""
+    #Verify Server address and contact server
     initContact(args, s)
 
-    """Get server response"""
+    #Get server response
     processInput(s)
 
-    """Conversation loop"""
+    #Conversation loop"""
     while 1:
-        """Get cli input and send to server"""
+        #Get cli input and send to server
         prepareOutput(s, handle)
 
-        """Process server response and print to terminal"""
+        #Process server response and print to terminal
         processInput(s)
         
                         
 def exitCleanly(signum, frame):
-    """Handles SIGINT and SIGTERM"""
-    
+    #Handles SIGINT and SIGTERM
     try:
         print("bye")
         s.shutdown()
@@ -66,18 +63,25 @@ def exitCleanly(signum, frame):
         exit(1)
 
 def cliHandler():
-    """"""
+    #Use argparse library to process cli
+    
     #Help obtained from python docs at
     #docs.python.org/3/library/argparse.html
     parser = argparse.ArgumentParser(description='Handle user port input')
-    parser.add_argument('hostname',
-                        type=str,
-                        help='The server\'s hostname(default = \'localhost\')')
+    parser.add_argument(
+        'hostname',
+        type=str,
+        help='The server\'s hostname(default = \'localhost\')')
     parser.add_argument('port', type=int, help='The server\'s port number')
     return parser.parse_args()
 
+
 def getHandle():
+    #Get user input for username
+    #@return padded username string of length 10
+    
     name = raw_input("Enter a handle: ")
+
     #ensure handle is 10 characters long
     if (len(name) > 10):
         name = name[:10]
@@ -88,7 +92,10 @@ def getHandle():
     return name
 
 def initContact(args, s):
-
+    #Establish connection with server
+    #@param args the cli arguments of hostname and port
+    #@param s the tcp socket object
+    
     #This error handling code heavily influenced by the accepted answer here:
     #stackoverflow.com/questions/177389/testing-socket-connection-in-python
     try:
@@ -100,13 +107,11 @@ def initContact(args, s):
         #print(e)
         s.close()
         exit(1)
-
-def encodeMSG(msg):
-    #spaces = MSG_BYTE_SIZE - len(msg)
-    #msg += '_' * spaces
-    return msg.encode('utf-8')
     
 def processInput(s):
+    #Receive and process a server message
+    #@param s the socket object
+    
     try:
         data = s.recv(1024)
     except socket.error as e:
@@ -118,29 +123,35 @@ def processInput(s):
         else:
             s.close()
             exit(1)
-      
+  
     data = data.decode('utf-8')
-    if len(data) < 10:
+    if len(data) < 10:  #Not a complete message
         print("server disconnected")
         s.close()
         exit(1)
-    servername = data[:10]
-    msg = data[10:data.find(MSG_END)]
-    print(servername.replace('_', '') + "> " + msg);
+    servername = data[:10]  #get server's username
+    msg = data[10:data.find(MSG_END)]  #remove ending tag from message
+    print(servername.replace('_', '') + "> " + msg);  #remove underscore padding
 
 def prepareOutput(s, handle):
+    #Get user input and send to server
+    #@param s the socket object
+    #@param handle the user's username
     try:
+        #get user inputted message
         uinput = raw_input(handle.replace('_', '') + '> ')
     except:
+        #close program on error
         s.close()
         exit(1)
-    if (uinput.find('\quit') != -1):
+    if (uinput.find('\quit') != -1):  #user typed '\quit'
         s.close()
         exit(1)
     else:
-        try:
-            s.sendall(encodeMSG(handle + uinput + MSG_END + "\n"))
+        try:  #compose and send message
+            s.sendall((handle + uinput + MSG_END + "\n").encode('utf-8'))
         except socket.error as e:
+            #close program if error sending
             if (e.errno == errno.ECONNRESET or e.errno == errno.ECONNABORTED):
                 print("server disconnected")
                 s.close()
